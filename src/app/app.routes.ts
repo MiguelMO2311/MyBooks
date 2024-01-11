36ee0033ebdd34ee7f9eca21aba57e29951d444c
MiguelMO2311 <mmeneses73@gmail.com>
2024-01-11 23:45:46 +0100

    ok retos tema 2 angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/Home', pathMatch: 'full' },
     { path: 'Home', component: HomeComponent },
     { path: 'Register', component: RegisterComponent },
+    { path: 'Profile', component: ProfileComponent },
 ];
