5fee99f718eb01dcc8b88936fd588a20fa5769d8
MiguelMO2311 <mmeneses73@gmail.com>
2024-01-26 19:01:12 +0100

    arreglos al proyecto dia4 padre hijo angular

--- src/app/component/cards/cards.component.ts
+++ src/app/component/cards/cards.component.ts
@@ -30,16 +30,9 @@ export class CardsComponent {
 
 
 
-@Input() public book: number = Number();
+@Input() public book: Books = new Books()
 
-@Input() public id_book: number = 0;
-@Input() public id_user: number = 0;
-@Input() public title: string = '';
-@Input() public type: string = '';
-@Input() public author: string = '';
-@Input() public price: number = 0;
-@Input() public photo: string = '';
-@Input() public library: any;
+
 
 @Output() eventDelete = new EventEmitter<number>();
 
